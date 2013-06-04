09106524fe3fe935a83f0f21774123702db6df44
David Dehghan <ddehghan@gmail.com>
2013-06-04 03:21:58 -0700

    major perf improvement. will not downlod the data files more than 1 time.

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,7 +1,61 @@
 //    http://www.schneidy.com/Tutorials/MapsTutorial.html
 
 
-MEDICARE.data = {};
+MEDICARE.data = undefined;
+MEDICARE.USMap = undefined;
+MEDICARE.drawRequests = [];
+
+var drawDataPoints = function (data, scale, col_name, svg) {
+
+    var projection = d3.geo.albersUsa();
+
+    var group = svg.append("g");
+    group.attr('transform', scale);
+
+    group.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) {
+            return projection([d.lon, d.lat])[0];
+        })
+        .attr("cy", function (d) {
+            return projection([d.lon, d.lat])[1];
+        })
+        .attr("r", function (d) {
+            return d[col_name];
+        })
+        .style("fill", "red")
+        .style("opacity", .5)
+        .style("stroke", "#2F0000")
+        .on("mouseover", function () {
+            d3.select(this).style("fill", "blue");
+        })
+        .on("mouseout", function () {
+            d3.select(this).style("fill", "red");
+        });
+
+    $('svg circle').tipsy({
+        gravity: 'w',
+        html: true,
+        title: function () {
+            var d = this.__data__;
+
+            var data = {
+                'd_name': d.name,
+                'd_charge': MEDICARE.formatMoney(d.charge, 0, '.', ','),
+                'd_pay': MEDICARE.formatMoney(d.pay, 0, '.', ','),
+                'AcquiredInfect': d.AcquiredInfect,
+                'AcquiredConditions': d.AcquiredConditions,
+                'PatientSafetySummary': d.PatientSafetySummary
+            };
+
+            var template = $('#tooltip-template').html();
+            var compiledTemplate = Handlebars.compile(template);
+            return compiledTemplate(data);
+        }
+    });
+};
 
 MEDICARE.draw_chart = function (chart_position, dataUrl, col_name) {
     var centered;
@@ -10,83 +64,68 @@ MEDICARE.draw_chart = function (chart_position, dataUrl, col_name) {
 
     $(chart_position + " svg").remove();
 
-
     var svg = d3.select(chart_position).append("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("class", 'chart-svg');
 
-    var group = svg.append("g");
     var projection = d3.geo.albersUsa();
     var path = d3.geo.path().projection(projection);
 
-    group.attr('transform', scale);
 
-    d3.json('/static/json/us-states.json', function (collection) {
-        group.selectAll('path')
-            .data(collection.features)
-            .enter().append('path')
-            .attr('d', d3.geo.path().projection(projection))
-            .attr('id', function (d) {
-                return d.properties.name.replace(/\s+/g, '')
-            })
-            .style('fill', 'gray')
-            .style('stroke', 'white')
-            .style('stroke-width', 1);
+    if (MEDICARE.USMap === undefined) {
+        var defs = svg.append("defs");
+        var group = defs.append("g");
+        group.attr('transform', scale);
+        group.attr('id', 'usmap-svg');
+
+        d3.json('/static/json/us-states.json', function (collection) {
+            group.selectAll('path')
+                .data(collection.features)
+                .enter().append('path')
+                .attr('d', d3.geo.path().projection(projection))
+                .attr('id', function (d) {
+                    return d.properties.name.replace(/\s+/g, '')
+                })
+                .style('fill', 'gray')
+                .style('stroke', 'white')
+                .style('stroke-width', 1);
+        });
+
+        var use = svg.append("use");
+        use.attr('xlink:href', '#usmap-svg');
+        MEDICARE.USMap = true;
+    }
+    else {
+        var use = svg.append("use");
+        use.attr('xlink:href', '#usmap-svg');
+    }
+
+    if (MEDICARE.data === undefined) {
+        console.log("server");
+
+        MEDICARE.data = null;
 
         d3.csv(dataUrl, function (error, data) {
-
             MEDICARE.data = data;
 
-            group = svg.append("g");
-            group.attr('transform', scale);
-
-            group.selectAll("circle")
-                .data(data)
-                .enter()
-                .append("circle")
-                .attr("cx", function (d) {
-                    return projection([d.lon, d.lat])[0];
-                })
-                .attr("cy", function (d) {
-                    return projection([d.lon, d.lat])[1];
-                })
-                .attr("r", function (d) {
-                    return d[col_name];
-                })
-                .style("fill", "red")
-                .style("opacity", .5)
-                .style("stroke", "#2F0000")
-                .on("mouseover", function () {
-                    d3.select(this).style("fill", "blue");
-                })
-                .on("mouseout", function () {
-                    d3.select(this).style("fill", "red");
-                });
-
-            $('svg circle').tipsy({
-                gravity: 'w',
-                html: true,
-                title: function () {
-                    var d = this.__data__;
-
-                    var data = {
-                        'd_name': d.name,
-                        'd_charge': MEDICARE.formatMoney(d.charge, 0, '.', ','),
-                        'd_pay': MEDICARE.formatMoney(d.pay, 0, '.', ','),
-                        'AcquiredInfect': d.AcquiredInfect,
-                        'AcquiredConditions': d.AcquiredConditions,
-                        'PatientSafetySummary': d.PatientSafetySummary
-                    };
-
-                    var template = $('#tooltip-template').html();
-                    var compiledTemplate = Handlebars.compile(template);
-                    return compiledTemplate(data);
-                }
+            MEDICARE.drawRequests.push(function () {
+                drawDataPoints(MEDICARE.data, scale, col_name, svg);
             });
 
+            while (MEDICARE.drawRequests.length > 0) {
+                MEDICARE.drawRequests.pop()();
+            }
         });
-    });
+    }
+    else {
+        console.log("local");
+
+        MEDICARE.drawRequests.push(function () {
+            drawDataPoints(MEDICARE.data, scale, col_name, svg);
+        });
+    }
+
 };
 
 
--- website/static/js/plot.js
+++ website/static/js/plot.js
@@ -1,4 +1,4 @@
-MEDICARE.plotXY = function (chart_position, dataUrl) {
+var drawDataPointsXY = function (data, chart_position) {
     $(chart_position + " svg").remove();
 
     var margin = {top: 20, right: 40, bottom: 30, left: 60},
@@ -27,112 +27,111 @@ MEDICARE.plotXY = function (chart_position, dataUrl) {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.csv(dataUrl, function (error, data) {
-        data.forEach(function (d) {
-            d.charge = +d.charge;
-            d.pay = +d.pay;
-        });
+    data.forEach(function (d) {
+        d.charge = +d.charge;
+        d.pay = +d.pay;
+    });
 
-        x.domain(d3.extent(data, function (d) {
-            return d.charge;
-        })).nice();
-        y.domain(d3.extent(data, function (d) {
-            return d.pay;
-        })).nice();
+    x.domain(d3.extent(data, function (d) {
+        return d.charge;
+    })).nice();
+    y.domain(d3.extent(data, function (d) {
+        return d.pay;
+    })).nice();
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-            .append("text")
-            .attr("class", "label")
-            .attr("x", width)
-            .attr("y", -6)
-            .style("text-anchor", "end")
-            .text("Hospital Bill");
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Hospital Bill");
 
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
-            .append("text")
-            .attr("class", "label")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Medicare Reimbursement");
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Medicare Reimbursement");
 
-        svg.selectAll(".dot")
-            .data(data)
-            .enter().append("circle")
-            .attr("class", "dot")
-            .attr("r", function (d) {
-                return 3; //d.petalLength;
-            })
-            .attr("cx", function (d) {
-                return x(d.charge);
-            })
-            .attr("cy", function (d) {
-                return y(d.pay);
-            })
-            .style("fill", "red");
+    svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", function (d) {
+            return 3; //d.petalLength;
+        })
+        .attr("cx", function (d) {
+            return x(d.charge);
+        })
+        .attr("cy", function (d) {
+            return y(d.pay);
+        })
+        .style("fill", "red");
 //            .style("fill", function (d) {
 //                return '#ccc';
 //                color(d.species);
 //            });
 
 
-        var legend = svg.selectAll(".legend")
-            .data(color.domain())
-            .enter().append("g")
-            .attr("class", "legend")
-            .attr("transform", function (d, i) {
-                return "translate(0," + i * 20 + ")";
-            });
-
-        legend.append("rect")
-            .attr("x", width - 18)
-            .attr("width", 18)
-            .attr("height", 18)
-            .style("fill", color);
-
-        legend.append("text")
-            .attr("x", width - 24)
-            .attr("y", 9)
-            .attr("dy", ".35em")
-            .style("text-anchor", "end")
-            .text(function (d) {
-                return d;
-            });
-
-
-        $('svg circle').tipsy({
-            gravity: 'w',
-            html: true,
-            title: function () {
-                var d = this.__data__;
-
-                var data = {
-                    'd_name': d.name,
-                    'd_charge': MEDICARE.formatMoney(d.charge, 0, '.', ','),
-                    'd_pay': MEDICARE.formatMoney(d.pay, 0, '.', ','),
-                    'AcquiredInfect': d.AcquiredInfect,
-                    'AcquiredConditions': d.AcquiredConditions,
-                    'PatientSafetySummary': d.PatientSafetySummary
-                };
-
-                var template = $('#tooltip-template').html();
-                var compiledTemplate = Handlebars.compile(template);
-
-                return compiledTemplate(data);
-
-            }
+    var legend = svg.selectAll(".legend")
+        .data(color.domain())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) {
+            return "translate(0," + i * 20 + ")";
         });
+
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function (d) {
+            return d;
+        });
+
+
+    $('svg circle').tipsy({
+        gravity: 'w',
+        html: true,
+        title: function () {
+            var d = this.__data__;
+
+            var data = {
+                'd_name': d.name,
+                'd_charge': MEDICARE.formatMoney(d.charge, 0, '.', ','),
+                'd_pay': MEDICARE.formatMoney(d.pay, 0, '.', ','),
+                'AcquiredInfect': d.AcquiredInfect,
+                'AcquiredConditions': d.AcquiredConditions,
+                'PatientSafetySummary': d.PatientSafetySummary
+            };
+
+            var template = $('#tooltip-template').html();
+            var compiledTemplate = Handlebars.compile(template);
+
+            return compiledTemplate(data);
+
+        }
     });
+
 };
 
 
-MEDICARE.costVsQuality = function (chart_position, dataUrl, col_name) {
+var costVsQuality = function (data, chart_position, col_name) {
     $(chart_position + " svg").remove();
 
     var margin = {top: 20, right: 40, bottom: 30, left: 60},
@@ -161,103 +160,159 @@ MEDICARE.costVsQuality = function (chart_position, dataUrl, col_name) {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.csv(dataUrl, function (error, data) {
-        data.forEach(function (d) {
-            d.charge =+ d.charge;
-            d.AcquiredConditions =+ d.AcquiredConditions;
+    data.forEach(function (d) {
+        d.charge = +d.charge;
+        d.AcquiredConditions = +d.AcquiredConditions;
+    });
+
+    x.domain(d3.extent(data, function (d) {
+        return d.charge;
+    })).nice();
+    y.domain(d3.extent(data, function (d) {
+        return d.AcquiredConditions;
+    })).nice();
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text("Cost");
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Quality");
+
+    svg.selectAll(".dot")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("class", "dot")
+        .attr("r", function (d) {
+            return 3; //d.petalLength;
+        })
+        .attr("cx", function (d) {
+            return x(d.charge);
+        })
+        .attr("cy", function (d) {
+            return y(d[col_name]);
+        })
+        .style("fill", "blue");
+
+
+    var legend = svg.selectAll(".legend")
+        .data(color.domain())
+        .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d, i) {
+            return "translate(0," + i * 20 + ")";
         });
 
-        x.domain(d3.extent(data, function (d) {
-            return d.charge;
-        })).nice();
-        y.domain(d3.extent(data, function (d) {
-            return d.AcquiredConditions;
-        })).nice();
+    legend.append("rect")
+        .attr("x", width - 18)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-            .append("text")
-            .attr("class", "label")
-            .attr("x", width)
-            .attr("y", -6)
-            .style("text-anchor", "end")
-            .text("Cost");
-
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
-            .append("text")
-            .attr("class", "label")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Quality");
-
-        svg.selectAll(".dot")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("class", "dot")
-            .attr("r", function (d) {
-                return 3; //d.petalLength;
-            })
-            .attr("cx", function (d) {
-                return x(d.charge);
-            })
-            .attr("cy", function (d) {
-                return y(d[col_name]);
-            })
-            .style("fill", "blue");
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function (d) {
+            return d;
+        });
 
 
-        var legend = svg.selectAll(".legend")
-            .data(color.domain())
-            .enter().append("g")
-            .attr("class", "legend")
-            .attr("transform", function (d, i) {
-                return "translate(0," + i * 20 + ")";
+    $('svg circle').tipsy({
+        gravity: 'w',
+        html: true,
+        title: function () {
+            var d = this.__data__;
+
+            var data = {
+                'd_name': d.name,
+                'd_charge': MEDICARE.formatMoney(d.charge, 0, '.', ','),
+                'd_pay': MEDICARE.formatMoney(d.pay, 0, '.', ','),
+                'AcquiredInfect': d.AcquiredInfect,
+                'AcquiredConditions': d.AcquiredConditions,
+                'PatientSafetySummary': d.PatientSafetySummary
+            };
+
+            var template = $('#tooltip-template').html();
+            var compiledTemplate = Handlebars.compile(template);
+
+            return compiledTemplate(data);
+
+        }
+    });
+
+};
+
+MEDICARE.downloadAndPaint = function (type, chart_position, dataUrl) {
+
+    if (MEDICARE.data === undefined) {
+        console.log("server");
+
+        MEDICARE.data = null;
+
+        d3.csv(dataUrl, function (error, data) {
+            MEDICARE.data = data;
+
+            MEDICARE.drawRequests.push(function () {
+                type(MEDICARE.data, chart_position);
             });
 
-        legend.append("rect")
-            .attr("x", width - 18)
-            .attr("width", 18)
-            .attr("height", 18)
-            .style("fill", color);
-
-        legend.append("text")
-            .attr("x", width - 24)
-            .attr("y", 9)
-            .attr("dy", ".35em")
-            .style("text-anchor", "end")
-            .text(function (d) {
-                return d;
-            });
-
-
-        $('svg circle').tipsy({
-            gravity: 'w',
-            html: true,
-            title: function () {
-                var d = this.__data__;
-
-                var data = {
-                    'd_name': d.name,
-                    'd_charge': MEDICARE.formatMoney(d.charge, 0, '.', ','),
-                    'd_pay': MEDICARE.formatMoney(d.pay, 0, '.', ','),
-                    'AcquiredInfect': d.AcquiredInfect,
-                    'AcquiredConditions': d.AcquiredConditions,
-                    'PatientSafetySummary': d.PatientSafetySummary
-                };
-
-                var template = $('#tooltip-template').html();
-                var compiledTemplate = Handlebars.compile(template);
-
-                return compiledTemplate(data);
-
+            while (MEDICARE.drawRequests.length > 0) {
+                MEDICARE.drawRequests.pop()();
             }
         });
-    });
+    }
+    else {
+        console.log("local");
+
+        MEDICARE.drawRequests.push(function () {
+            type(MEDICARE.data, chart_position);
+        });
+    }
 };
+
+MEDICARE.downloadAndPaint2 = function (type, chart_position, dataUrl, col_name) {
+
+    if (MEDICARE.data === undefined) {
+        console.log("server");
+
+        MEDICARE.data = null;
+
+        d3.csv(dataUrl, function (error, data) {
+            MEDICARE.data = data;
+
+            MEDICARE.drawRequests.push(function () {
+                type(MEDICARE.data, chart_position, col_name);
+            });
+
+            while (MEDICARE.drawRequests.length > 0) {
+                MEDICARE.drawRequests.pop()();
+            }
+        });
+    }
+    else {
+        console.log("local");
+
+        MEDICARE.drawRequests.push(function () {
+            type(MEDICARE.data, chart_position, col_name);
+        });
+    }
+};
+
