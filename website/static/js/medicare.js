a9e8fe318130fbcec87240d84a979085ee653643
David Dehghan <ddehghan@gmail.com>
2013-06-13 04:12:38 -0700

    clean up

--- website/static/js/medicare.js
+++ website/static/js/medicare.js
@@ -1,16 +1,5 @@
 var MYCHART = {'init': null, 'paint': null};
 
-//MEDICARE.get_drg = function () {
-//    $(".chart-svg circle").remove();
-//
-//    var e = document.getElementById("drug_name");
-//
-//    $(".selected_drg").html(e.options[e.selectedIndex].text);
-//
-//    MEDICARE.DRGData.download = "not-started";
-//
-//    return e.options[e.selectedIndex].value;
-//};
 
 MEDICARE.get_data = function (drg_num, drg_name) {
     $(".chart-svg circle").remove();
@@ -24,23 +13,3 @@ MEDICARE.get_data = function (drg_num, drg_name) {
     MYCHART.init(data_rul);
 
 };
-
-//MEDICARE.dataUrl = function () {
-//    return '/static/data/' + MEDICARE.get_drg() + '.csv'
-//};
-
-//MEDICARE.LoadOptions = function (callback) {
-//    $.getJSON('/static/data/drg_options.json', function (jsonData) {
-//        $.each(jsonData, function (i, j) {
-//            $('#drug_name').append($('<option></option>').val(j.value).html(j.text));
-//        });
-//
-//        callback(MEDICARE.dataUrl());
-//    });
-//};
-
-
-MYCHART.paint = function () {
-    MYCHART.init(MEDICARE.dataUrl());
-};
-
